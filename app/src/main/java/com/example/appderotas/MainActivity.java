16fffdda8871c2fa40aff3089bb6397c580e1d0a
Thiago Lima <thiagobenedictis@gmail.com>
2025-11-26 12:56:15 -0300

    Primeiro commit

--- app/src/main/java/com/example/appderotas/MainActivity.java
+++ app/src/main/java/com/example/appderotas/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.appderotas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+private Intent maps;
+private Button iniciar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        maps = new Intent(this, MapsActivity.class);
+        iniciar = findViewById(R.id.Iniciar);
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(maps);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
